49c97a066edf1bd61a92f46e092ff4660dae1ec3
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-05-16 04:12:53 +0900

    [Passed] 두 개의 격자 비교하기 (46ms, 16MB)

---
+++ trail0/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+b1 = [list(map(int, input().split())) for _ in range(n)]
+b2 = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n) :
+    for j in range(m)  :
+        if b1[i][j] == b2[i][j] :
+            print(0, end=' ')
+        else:
+            print(1, end=' ')
+    print()
+        
